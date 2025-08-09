54fe23de2724f6ce6aab4c23d44bd313acc2dc69
Cherry-Machado <cherry5467@gmail.com>
2025-08-09 00:26:04 -0300

    Updating

--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -6,28 +6,64 @@ const messageController = require("../controllers/messageController");
 const messageValidation = require("../utilities/message-validation");
 const utilities = require("../utilities");
 
-router.use(["/view/:messageId", "/compose", "/compose/:messageId", "/send", "/archive", "/view/:messageId/delete", "/delete", "/view/:messageId/toggle-read", "/view/:messageId/toggle-archived"], utilities.checkLogin);
-
+// Route to build the main inbox view
 router.get("/", utilities.checkLogin, utilities.handleErrors(messageController.buildInbox));
 
 // Route to build message view view
-router.get("/view/:messageId", utilities.handleErrors(messageController.buildMessageView));
+router.get(
+  "/view/:messageId",
+  utilities.checkLogin,
+  utilities.handleErrors(messageController.buildMessageView)
+);
 
 // Route to build compose messages view
-router.get("/compose", utilities.handleErrors(messageController.buildCompose));
-router.get("/compose/:messageId", utilities.handleErrors(messageController.buildCompose));
-router.post("/send", messageValidation.sendMessageRules(), messageValidation.checkMessageData, utilities.handleErrors(messageController.sendMessage))
+router.get(
+  "/compose",
+  utilities.checkLogin,
+  utilities.handleErrors(messageController.buildCompose)
+);
+router.get(
+  "/compose/:messageId",
+  utilities.checkLogin,
+  utilities.handleErrors(messageController.buildCompose)
+);
+router.post(
+  "/send",
+  utilities.checkLogin,
+  messageValidation.sendMessageRules(),
+  messageValidation.checkMessageData,
+  utilities.handleErrors(messageController.sendMessage)
+);
 
 // Rout to build archived messages view
-router.get("/archive", utilities.handleErrors(messageController.buildArchive));
+router.get(
+  "/archive",
+  utilities.checkLogin,
+  utilities.handleErrors(messageController.buildArchive)
+);
 
 // Route to build delete message confirmation view
-router.get("/view/:messageId/delete", utilities.handleErrors(messageController.buildDelete));
-router.post("/delete", utilities.handleErrors(messageController.deleteMessage));
-
+router.get(
+  "/view/:messageId/delete",
+  utilities.checkLogin,
+  utilities.handleErrors(messageController.buildDelete)
+);
+router.post(
+  "/delete",
+  utilities.checkLogin,
+  utilities.handleErrors(messageController.deleteMessage)
+);
 
 //API calls
-router.get("/view/:messageId/toggle-read", utilities.handleErrors(messageController.toggleRead));
-router.get("/view/:messageId/toggle-archived", utilities.handleErrors(messageController.toggleArchived));
+router.post(
+  "/view/:messageId/toggle-read",
+  utilities.checkLogin,
+  utilities.handleErrors(messageController.toggleRead)
+);
+router.post(
+  "/view/:messageId/toggle-archived",
+  utilities.checkLogin,
+  utilities.handleErrors(messageController.toggleArchived)
+);
 
 module.exports = router;
