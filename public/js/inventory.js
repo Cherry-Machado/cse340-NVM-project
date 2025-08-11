9925ddace9dc18837a19f9221eb377d8870a890c
Cherry-Machado <cherry5467@gmail.com>
2025-08-10 22:35:21 -0300

    Updating

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -5,7 +5,6 @@ classificationList.addEventListener("change", function () {
   let classification_id = classificationList.value;
   console.log(`classification_id is: ${classification_id}`);
   let classIdURL = "/inv/getInventory/" + classification_id;
-  console.log(classIdURL);
   fetch(classIdURL)
     .then(function (response) {
       if (response.ok) {
@@ -14,7 +13,6 @@ classificationList.addEventListener("change", function () {
       throw Error("Network response was not OK");
     })
     .then(function (data) {
-      console.log(data);
       buildInventoryList(data);
     })
     .catch(function (error) {
@@ -35,7 +33,6 @@ function buildInventoryList(data) {
   dataTable += "<tbody>";
   // Iterate over all vehicles in the array and put each in a row
   data.forEach(function (element) {
-    console.log(element.inv_id + ", " + element.inv_model);
     dataTable += `<tr><td>${element.inv_make} ${element.inv_model}</td>`;
     dataTable += `<td><a href='/inv/edit/${element.inv_id}' title='Click to update'>Modify</a></td>`;
     dataTable += `<td><a href='/inv/delete/${element.inv_id}' title='Click to delete'>Delete</a></td></tr>`;
